7d6b17fb6f7a0a2fe2faa6fc87929188df8ec8fc
MongooseFox <jakstroinski@gmail.com>
2022-06-20 00:01:10 +0200

    main

---
+++ Defender/Assets/StarSparrow/Scripts/PlayerControls.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControls : MonoBehaviour
+{
+    [SerializeField] InputAction movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // float horizontalThrow = Input.GetAxis("Horizontal");
+        // Debug.Log("Horizontal");
+        // float verticalThrow = Input.GetAxis("Vertical");
+        // Debug.Log("Vertical");
+    }
+}
